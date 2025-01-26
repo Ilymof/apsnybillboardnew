ad052d21f083c175a3b493d0e8133ddb13609757
YomoABH <yomoyomosai@gmail.com>
2025-01-26 06:49:57 +0300

    refac: Изменить таблицу users

--- src/migrations/003_users_migration.sql
+++ src/migrations/003_users_migration.sql
@@ -9,11 +9,12 @@ CREATE TABLE users (
     whatsapp VARCHAR(255), 
     ip VARCHAR(45), 
     useragent TEXT, 
+    auth_provider VARCHAR(50),
+    provider_user_id VARCHAR(255), 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
-INSERT INTO users (full_name, role_id, phone, telegram, whatsapp, ip, useragent)
+INSERT INTO users (full_name, role_id, phone, telegram, whatsapp, ip, useragent, auth_provider, provider_user_id)
 VALUES
-('Иван Иванов', 1, '+79991234567', '@ivanivanov', '+79990001234', '192.168.1.1', 'Mozilla/5.0');
+('Иван Иванов', 1, '+79991234567', '@ivanivanov', '+79990001234', '192.168.1.1', 'Mozilla/5.0', 'telegram', '123456789'); 
